fe5957208cb58b9bc5cfcfb075aa6e07ff127bed
spyderpz2 <hhmeijden@gmail.com>
2020-05-08 11:38:49 +0200

    Added README

--- src/Application.kt
+++ src/Application.kt
@@ -11,14 +11,16 @@ import kotlinx.css.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>){
+    val Unit = io.ktor.server.netty.EngineMain.main(args)
+}
+
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     val client = HttpClient(Apache) {
     }
-
     routing {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
